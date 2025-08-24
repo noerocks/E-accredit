1fe83885cadedbb95f0d6d0170278be97a584790
Noe Villamor <noejayar@gmail.com>
2025-08-24 23:41:42 +0800

    Landing page initial header and hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,61 @@
 import Image from "next/image";
+import { IBM_Plex_Serif } from "next/font/google";
+
+const IBMPlexSerif = IBM_Plex_Serif({
+  subsets: ["latin"],
+  weight: "700",
+});
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+    <div>
+      <header className="bg-[#f9a602] px-10 h-[100px] flex justify-between items-center">
+        <div className="flex space-x-2 items-center">
+          <div className="h-[70px] aspect-square rounded-full overflow-hidden">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src={"/ctu-logo.png"}
+              alt="ctu logo"
+              width={70}
+              height={70}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </div>
+          <div>
+            <p
+              className={`${IBMPlexSerif.className} antialiased text-xl text-gray-800`}
+            >
+              <span className="text-2xl">C</span>EBU{" "}
+              <span className="text-2xl">T</span>ECHNOLOGICAL{" "}
+              <span className="text-2xl">U</span>NIVERSITY -{" "}
+              <span className="text-2xl">N</span>AGA
+            </p>
+            <p className="text-lg">Accreditation System</p>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </header>
+      <nav className="sticky top-0 h-[40px] w-ful z-10 shadow-md shadow-gray-400">
+        <ul className="flex space-x-20 justify-center items-center bg-[#282828] text-white h-full">
+          <li>
+            <a href="#">E-accredit</a>
+          </li>
+          <li>
+            <a href="#features">Features</a>
+          </li>
+          <li>How it Works</li>
+          <li>About</li>
+        </ul>
+      </nav>
+      <section className="h-[calc(100vh-140px)]">
+        <Image
+          src={"/ctu-building.jpg"}
+          alt="ctu naga building"
+          width={1440}
+          height={456}
+          className="w-full h-full object-cover object-[0%_90%] opacity-40"
+        />
+      </section>
+      <section id="features" className="h-[calc(100vh-40px)]">
+        features
+      </section>
     </div>
   );
 }
