18ace98c34296e7a86755d5e33c4656d41cbc524
Noe Villamor <noejayar@gmail.com>
2025-09-11 11:05:28 +0800

    Isolated add program personnel form as client component and converted program personnel list as server component

---
+++ components/admin/program/add-program-personnell-form.tsx
@@ -0,0 +1,107 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { DialogClose, DialogFooter } from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Tooltip, TooltipTrigger } from "@/components/ui/tooltip";
+import { AddProgramPersonnelFormSchema } from "@/lib/zod-definitions";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { UserPlus } from "lucide-react";
+import { useTransition } from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import z from "zod";
+import { AddProgramPersonnel } from "@/lib/action/program-personnel";
+import { UsersDTO } from "@/lib/dto/user";
+
+const AddProgramPersonnelForm = ({
+  programId,
+  accreditationOfficers,
+}: {
+  programId: string;
+  accreditationOfficers: UsersDTO[] | null;
+}) => {
+  const form = useForm<z.infer<typeof AddProgramPersonnelFormSchema>>({
+    resolver: zodResolver(AddProgramPersonnelFormSchema),
+    defaultValues: {
+      userId: "",
+    },
+  });
+  const [pending, startTransition] = useTransition();
+  const onSubmit = (data: z.infer<typeof AddProgramPersonnelFormSchema>) => {
+    startTransition(async () => {
+      const result = await AddProgramPersonnel.bind(null, programId)(data);
+      switch (result?.status) {
+        case "success":
+          toast.success(result.message);
+          break;
+        case "error":
+          toast.error(result.message);
+          break;
+      }
+    });
+  };
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          control={form.control}
+          name="userId"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Add Program Personnel</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger className="w-full">
+                    <SelectValue placeholder="Choose accreditation officer" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  {accreditationOfficers?.map((officer) => (
+                    <SelectItem
+                      value={officer.id}
+                      key={officer.id}
+                    >{`${officer.firstName} ${officer.lastName}`}</SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+              <FormDescription>
+                Assign an accreditation officer to this program
+              </FormDescription>
+              <FormMessage />
+              <DialogFooter>
+                <DialogClose asChild>
+                  <Button variant="outline">Cancel</Button>
+                </DialogClose>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Button>
+                      <UserPlus /> Add
+                    </Button>
+                  </TooltipTrigger>
+                </Tooltip>
+              </DialogFooter>
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
+  );
+};
+
+export default AddProgramPersonnelForm;
--- components/admin/program/program-personell-list.tsx
+++ components/admin/program/program-personell-list.tsx
@@ -1,33 +1,5 @@
-"use client";
-
-import { Button } from "@/components/ui/button";
-import { DialogClose, DialogFooter } from "@/components/ui/dialog";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-import { Tooltip, TooltipTrigger } from "@/components/ui/tooltip";
-import { AddProgramPersonnel } from "@/lib/action/program-personnel";
 import { UsersDTO } from "@/lib/dto/user";
-import { AddProgramPersonnelFormSchema } from "@/lib/zod-definitions";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { UserPlus } from "lucide-react";
-import { useTransition } from "react";
-import { useForm } from "react-hook-form";
-import { toast } from "sonner";
-import z from "zod";
+import AddProgramPersonnelForm from "./add-program-personnell-form";
 
 const ProgramPersonnelList = ({
   params,
@@ -36,77 +8,13 @@ const ProgramPersonnelList = ({
   params: { id: string };
   accreditationOfficers: UsersDTO[] | null;
 }) => {
-  const form = useForm<z.infer<typeof AddProgramPersonnelFormSchema>>({
-    resolver: zodResolver(AddProgramPersonnelFormSchema),
-    defaultValues: {
-      userId: "",
-    },
-  });
-  const [pending, startTransition] = useTransition();
-  const onSubmit = (data: z.infer<typeof AddProgramPersonnelFormSchema>) => {
-    startTransition(async () => {
-      const result = await AddProgramPersonnel.bind(null, params.id)(data);
-      switch (result?.status) {
-        case "success":
-          toast.success(result.message);
-          break;
-        case "error":
-          toast.error(result.message);
-          break;
-      }
-    });
-  };
-
   return (
     <div>
       <p>{params.id}</p>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)}>
-          <FormField
-            control={form.control}
-            name="userId"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Add Program Personnel</FormLabel>
-                <Select
-                  onValueChange={field.onChange}
-                  defaultValue={field.value}
-                >
-                  <FormControl>
-                    <SelectTrigger className="w-full">
-                      <SelectValue placeholder="Choose accreditation officer" />
-                    </SelectTrigger>
-                  </FormControl>
-                  <SelectContent>
-                    {accreditationOfficers?.map((officer) => (
-                      <SelectItem
-                        value={officer.id}
-                        key={officer.id}
-                      >{`${officer.firstName} ${officer.lastName}`}</SelectItem>
-                    ))}
-                  </SelectContent>
-                </Select>
-                <FormDescription>
-                  Assign an accreditation officer to this program
-                </FormDescription>
-                <FormMessage />
-                <DialogFooter>
-                  <DialogClose asChild>
-                    <Button variant="outline">Cancel</Button>
-                  </DialogClose>
-                  <Tooltip>
-                    <TooltipTrigger asChild>
-                      <Button>
-                        <UserPlus /> Add
-                      </Button>
-                    </TooltipTrigger>
-                  </Tooltip>
-                </DialogFooter>
-              </FormItem>
-            )}
-          />
-        </form>
-      </Form>
+      <AddProgramPersonnelForm
+        programId={params.id}
+        accreditationOfficers={accreditationOfficers}
+      />
     </div>
   );
 };
