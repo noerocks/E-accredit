5325b06a5b1dbff9f3ca417db8685f93ba57c8a3
Noe Villamor <noejayar@gmail.com>
2025-09-13 11:36:20 +0800

    Intialize layout for instrument with sidebar and handled overflow

--- app/(dashboard)/admin/instruments/[id]/layout.tsx
+++ app/(dashboard)/admin/instruments/[id]/layout.tsx
@@ -1,13 +1,23 @@
-import { Sidebar, SidebarContent } from "@/components/ui/sidebar";
-import React from "react";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarInset,
+  SidebarProvider,
+} from "@/components/ui/sidebar";
 
 const InstrumentLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div>
-      <Sidebar>
+    <div className="flex h-full">
+      <Sidebar collapsible="none" className="bg-muted border-r overflow-auto">
+        <SidebarHeader></SidebarHeader>
         <SidebarContent></SidebarContent>
+        <SidebarFooter></SidebarFooter>
       </Sidebar>
-      {children}
+      <SidebarInset>
+        <div className="flex-1 h-full bg-muted overflow-auto">{children}</div>
+      </SidebarInset>
     </div>
   );
 };
