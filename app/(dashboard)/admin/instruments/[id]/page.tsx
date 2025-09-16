7142ac761e81b7496a8e82fe4285a9257d582db9
Noe Villamor <noejayar@gmail.com>
2025-09-16 11:13:48 +0800

    Instrument page section cards

--- app/(dashboard)/admin/instruments/[id]/page.tsx
+++ app/(dashboard)/admin/instruments/[id]/page.tsx
@@ -1,4 +1,15 @@
+import { Card, CardContent } from "@/components/ui/card";
 import { getInstrumentStructureById } from "@/lib/dal/instrument";
+import {
+  CheckCircle,
+  ClipboardList,
+  Folder,
+  Info,
+  Layers,
+  Pen,
+} from "lucide-react";
+import clsx from "clsx";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 
 const InstrumentPage = async ({
   params,
@@ -16,11 +27,55 @@ const InstrumentPage = async ({
     .flat()
     .map((parameter) => parameter.indicator.length)
     .reduce((total, indicatorCount) => (total += indicatorCount), 0);
+  const components = [
+    { label: "Areas", count: areaCount },
+    { label: "Parameters", count: parameterCount },
+    { label: "Indicators", count: indicatorCount },
+  ];
 
   return (
-    <div className="w-3/4 mt-10 mx-auto">
-      <p className="text-2xl">{instrument?.name}</p>
-      <p className="text-foreground">{instrument?.accreditingBody}</p>
+    <div className="w-3/4 mt-10 mx-auto flex flex-col gap-10">
+      <Card>
+        <CardContent className="flex flex-col gap-5">
+          <p className="text-3xl">{instrument?.name}</p>
+          <div className="flex justify-between items-center">
+            <p className="text-foreground">{instrument?.accreditingBody}</p>
+            <div>
+              <Pen size={20} />
+            </div>
+          </div>
+        </CardContent>
+      </Card>
+      <div className="flex flex-col gap-5">
+        <p className="text-2xl text-foreground">Components</p>
+        <div className="flex gap-5">
+          {components.map((component, index) => (
+            <Card key={index} className="flex-1">
+              <CardContent className="flex items-center gap-5">
+                <div
+                  className={clsx("p-4 rounded-full", {
+                    "bg-blue-400/30 text-blue-400": index === 0,
+                    "bg-green-400/30 text-green-400": index === 1,
+                    "bg-yellow-400/30 text-yellow-400": index === 2,
+                  })}
+                >
+                  {index === 0 ? (
+                    <Layers />
+                  ) : index === 1 ? (
+                    <ClipboardList />
+                  ) : (
+                    <CheckCircle />
+                  )}
+                </div>
+                <div>
+                  <p className="text-2xl font-bold">{component.count}</p>
+                  <p>{component.label}</p>
+                </div>
+              </CardContent>
+            </Card>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
